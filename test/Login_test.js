51ea031b880bf485f5bea5fdc3f6914015dfdece
SourMosquito <tania.martinez.mart@gmail.com>
2021-08-05 03:08:26 -0500

    Agregando ejercicios y primeras pruebas

---
+++ test/Login_test.js
@@ -0,0 +1,46 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+const url = "http://localhost:5000";
+
+describe("Registro y autenticación de usuarios", () => {
+
+    describe("Registro de usuario", () =>{
+        it("Debe registrar nuevo usuario", (done) =>{
+            chai.request(url)
+            .post('/users')
+            .send({
+                name: "Laura",
+                lastname: "Escobar",
+                email: "laura66@gmail.com",
+                password: "1234",
+            })
+            .end(function(err, res) {
+                //console.log(res);
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.property('user');
+                expect(res.body).to.have.property('message');
+                done();
+            });
+        });
+        it("Debe rechazar el registro de usuario existente", (done) => {
+            chai.request(url)
+            .post('/users')
+            .send({
+                name: "Laura",
+                lastname: "Escobar",
+                email: "laura66@gmail.com",
+                password: "1234",
+            })
+            .end(function(err, res) {
+                //console.log(res);
+                expect(res).to.have.status(400);
+                expect(res.body).to.have.property('error');
+                expect(res.body).to.have.property('message');
+                done();
+            });
+        });
+    });
+});
